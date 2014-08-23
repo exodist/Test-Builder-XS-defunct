3fd829a2ac9f205ddbc847a74ce2053da102e3be
Chad Granum <chad.granum@dreamhost.com>
2014-08-23 13:28:01 -0700

    Documentation

--- lib/Test/Builder/XS/Trace.pm
+++ lib/Test/Builder/XS/Trace.pm
@@ -17,3 +17,39 @@ sub transitions  { $_[0]->{transitions}  }
 sub tools        { $_[0]->{tools}        }
 
 1;
+
+__END__
+
+=pod
+
+=head1 name
+
+Test::Builder::XS::Trace - XS subclass of Test::Builder::Trace
+
+=head1 DESCRIPTION
+
+Some parts of Test::Builder are very slow in pure perl code. Recent updates to
+Test::Builder have almost doubled the time it takes for the perl test suite to
+complete. This module is an attempt to maintain performance without sacrificing
+capabilities.
+
+=head1 SEE ALSO
+
+L<Test::Builder::Trace> - This subclass does not implement any methods
+that are not already documented in the base class.
+
+=head1 AUTHORS
+
+Chad Granum L<exodist7@gmail.com>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2014 Chad Granum
+
+Test-Builder-XS is free software; Standard perl license (GPL and Artistic).
+
+Test-Builder-XS is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the license for more details.
+
+=cut
